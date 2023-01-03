7e80806b885361e905a40c615255c06e79f26d52
Tabar Purroy <N812100@admon-cfnavarra.es>
2023-01-03 22:38:11 +0100

    inputs created

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import "./Register.css"
 
 const Register = () => {
   return (
     <div className="registerPage">
       <h1>Register</h1>
+
+<div className="logoRegister">
+  {/* <img className ="logoDesign" src={} alt={} /> */}
+</div>
+
+<div className="registerInputs">
+<div>
+<input placeholder="Full name" type="text" name="name" />
+</div>
+<div>
+<input placeholder="Email" type="email" name="email" />
+</div>
+<div>
+<input placeholder="Phone number" type= "text" name="phone"/>
+</div>
+<div>
+<input placeholder="Password" type="password" name="password" />
+</div>
+<div>
+<input placeholder="Repeat password" type="password" name="password2" />
+</div>
+
+<div className="registerButton">Register</div>
+
+</div>
+
     </div>
   );
 };
