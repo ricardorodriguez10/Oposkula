9105d411b4b42d41f2a10bab8c8f7dc2f3e44ca3
Tabar Purroy <N812100@admon-cfnavarra.es>
2023-01-08 13:41:36 +0100

    inputs created with warning

---
+++ src/Services/validate.js
@@ -0,0 +1,45 @@
+
+export const errorValidate = (name,value) => {
+
+switch (name) {
+
+    case "name":
+
+        if (""){
+            return "Invalid name format";
+        } else {
+            return "";
+        }
+
+    case "email":
+
+    if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value) ){
+        return "Invalid e-mail format";
+    } else {
+        return "";
+    }
+        
+    case "phone":
+        if (! /(?=.*?[0-9])/.test(value)) {
+return "Phone number incorrect"
+        } else {
+            return "";
+        }
+
+        case "password":
+        case "password2":
+
+            if (value.length < 6) {
+    return "Write at least 6 characters"
+            } else {
+            if (! /[\d()+-]/g.test(value)) {
+                return "Invalid password format";
+            } else {
+                return "";
+            }
+        }
+    
+    default: console.log("Check that all the answers are correct" ); 
+}
+
+};
--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { errorValidate } from "../../Services/validate";
 import "./Register.css"
 
 const Register = () => {
+
+const [user, setUser] = useState({
+  name: "",
+  email: "",
+  phone: "",
+  password: "",
+  password2:"",
+})
+
+const [userError, setUserError] = useState ({
+  nameError: "",
+  emailError: "",
+  phoneError: "",
+  passwordError: "",
+  password2Error:"",
+})
+
+const handleChange = (event) => {
+
+const {name, value} = event.target;
+setUser (prevValue => {
+  return {
+  ...prevValue,
+  [name] : value
+}
+})
+}
+
+const errorHandler = (event) => {
+const {name, value} = event.target;
+let error = "";
+error = errorValidate (name, value);
+
+if (event.target.name === "password2") {
+  if (user.password !== user.password2) {
+    error = "Write the same password"
+  }
+}
+
+setUserError (prevValue => {
+  return {
+    ...prevValue, 
+    [name + "Error"]: error,
+  }
+})
+
+}
+
   return (
     <div className="registerPage">
       <h1>Register</h1>
@@ -11,21 +60,16 @@ const Register = () => {
 </div>
 
 <div className="registerInputs">
-<div>
-<input placeholder="Full name" type="text" name="name" />
-</div>
-<div>
-<input placeholder="Email" type="email" name="email" />
-</div>
-<div>
-<input placeholder="Phone number" type= "text" name="phone"/>
-</div>
-<div>
-<input placeholder="Password" type="password" name="password" />
-</div>
-<div>
-<input placeholder="Repeat password" type="password" name="password2" />
-</div>
+<input className={userError.nameError === "" ? "inputNormal" : "inputNormal inputError"} placeholder="Full name" type="text" name="name" onChange={handleChange} onBlur={errorHandler} />
+<div className="errorMessage"> {userError.nameError}</div>
+<input className={userError.emailError === "" ? "inputNormal" : "inputNormal inputError"} placeholder="Email" type="email" name="email" onChange={handleChange} onBlur={errorHandler}/>
+<div className="errorMessage"> {userError.emailError}</div>
+<input className={userError.phoneError === "" ? "inputNormal" : "inputNormal inputError"} placeholder="Phone number" type= "text" name="phone"  onChange={handleChange} onBlur={errorHandler}/>
+<div className="errorMessage"> {userError.phoneError}</div>
+<input className={userError.passwordError === "" ? "inputNormal" : "inputNormal inputError"} placeholder="Password" type="password" name="password" onChange={handleChange} onBlur={errorHandler} />
+<div className="errorMessage"> {userError.passwordError}</div>
+<input className={userError.password2Error === "" ? "inputNormal" : "inputNormal inputError"} placeholder="Repeat password" type="password" name="password2" onChange={handleChange} onBlur={errorHandler} />
+<div className="errorMessage"> {userError.password2Error}</div>
 
 <div className="registerButton">Register</div>
 
