baebfacc49755ab3ed03fabd39846cee1e436bcf
Tabar Purroy <N812100@admon-cfnavarra.es>
2023-05-11 20:17:13 +0200

    whatsapp button added

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,7 +5,6 @@ import phone from "../../assets/phone.jpg"
 import police from "../../assets/police.jpg"
 import firefighter from "../../assets/firefighter.jpg"
 import forestryAgent from "../../assets/forestryAgent.jpg"
-
 import sportnutritionist from "../../assets/sportnutritionist.jpg"
 import trainer from "../../assets/trainer.jpg"
 import "./Home.css"
@@ -16,20 +15,20 @@ import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Profile from "../Profile/Profile";
+import Button from 'react-bootstrap/Button';
 
  const Home = () => {
     return (
       <div className="homePage">
 
-       <section id="oppositions">
-
-       <div className="row">
+       <section className="oppositions">
 
        <div className="firstPart">
           <h1>Nutrición y Entrenamiento</h1>
           <h1>para Opositores</h1>
           </div>
-</div>
+          
+</section>
       
           <section className="whoIsOpox">
           <div className="whoIsOpoxContent">
@@ -86,7 +85,7 @@ import Profile from "../Profile/Profile";
       </Carousel.Item>
     </Carousel>
 
-       </section>
+       
 
        <section className="OurServices">
 
@@ -127,166 +126,22 @@ import Profile from "../Profile/Profile";
 
        </section>
 
-              {/* <CardGroup>
-      <Card>
-        <Card.Img variant="top" src={phone} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-           <p className="cardP"> This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.</p>
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src={sportnutritionist} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            <p className="cardP">This card has supporting text below as a natural lead-in to
-            additional content.</p>
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src={trainer} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-          <p className="cardP"> This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.</p>
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-       <Card>
-        <Card.Img variant="top" src={phone} />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-           <p className="cardP"> This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.</p>
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardGroup> */}
-
        
-        <section className="footer">
+        <section className="contactUs">
 
-          <div className="firstPartText">
-            <p>Tu nutricionista deportivo y entrenador
-          personalizaran y daran seguimiento a tu
-            planificacion nutricional y entrenamiento
-            a través de tu app </p>
+          <div className="lastPart">
+            <h2>¿Quieres más información?</h2>
+            <h3>¡Qué esperas, contacta con nosotros!</h3>
+            <a href="https://api.whatsapp.com/send?phone=34649689420&text=Hola,%20me%20podrías%20dar
+%20más%20información%20de%20los%20servicios%20que%20ofrecéis,%20muchas%20gracias.">
+            <Button variant="success">Contacto</Button>
+            </a>
           </div>
           
         </section>
-
-
-        <section id="ourCommitment">
         
-
-
-        <div className="howItWorks">
-          <h2>¿Cómo funciona OPOX?</h2>
-          <p>A través de una suscripción de pago a la app</p>
-        </div>
-       
-
-        <section className="completeYourProfile">
-
-        <div className="row">
-        
-        <div className=" phoneInfo col-lg-3 col-md-4">
-          <h1> Completaras tu perfil </h1>
-          <p>A mas datos de tu composición corporal, más especificos seremos y mejor rendimiento tendrás.</p>
-        </div>
-
-        <div className="phoneImg col-lg-3 col-md-4">
-        <img className="phoneImg" src={phone} alt="Oposiciones"></img>
-        </div>
-</div>
-
-
-        </section>
-
-        <section className="yourSportNutritionist">
-
-        <div className="row">
-
-        <div className="yourNutritionist col-lg-3 col-md-4">
-        <img className="nutritionistImg" src={sportnutritionist} alt="Nutritionist"></img>
-        </div>
-
-        <div className="nutritionistInfo col-lg-3 col-md-4">
-          <h1> Tú nutricionista deportivo</h1>
-          <p> elaborará un plan nutricional especifico dependiendo la exigencia de la prueba</p>
-        </div>
-</div>
-        </section>
-
-        
-        <section className="training">
-
-        <div className="row">
-
-        <div className="yourCoach col-lg-3 col-md-4">
-          <h1> Tú entrenador</h1>
-          <p> diseñará un plan de entrenamiento para tu oposición</p>
-        </div>
-
-        <div className="trainingInfo col-lg-3 col-md-4">
-        <img className="coachImg" src={trainer} alt="Training"></img>
-        </div>
-        
-        </div>
-        </section>
-
-        <div className="doubts col-lg-3 col-md-4">
-          <h1> 4: ¿Alguna duda?</h1>
-          <p>tu nutricionista y entrenador estarán dispuestos a resolver todas tus dudas en cualquier momento</p>
-        </div>
-        
-
-        </section>
-        
-
       </div>
        );
      };
 
      export default Home;
-
-// export default function FoodSearch () {
-//    const [meals, setMeals] = useState([]);
-//   const [search, setSearch] = useState("");
-
-
-// useEffect(() => {
-// getDataFromApi()
-// .then((result) => {
-//   const meals = result.data.hints
-//   setMeals(meals)
-// })
-// .catch((error) => console.log(error))
-// }, []);
-
-// return (
-//   <div className="homeDiv">
-// <FoodList food={meals} />
-//   </div>
-// )
-// }
